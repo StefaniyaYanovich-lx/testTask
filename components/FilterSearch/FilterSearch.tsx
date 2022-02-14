b1ddb43a06b95b61e3a10700f4bca0503194df46
StefaniyaYanovich-lx <stefaniya.yanovich@leverx.com>
2022-02-14 16:51:52 +0300

    feat: add filter

---
+++ components/FilterSearch/FilterSearch.tsx
@@ -0,0 +1,48 @@
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import SearchIcon from "@material-ui/icons/Search";
+import { TextField } from "@material-ui/core";
+import styles from "../FilterSearch/FilterSearch.module.css";
+import React, {Dispatch, SetStateAction, useEffect, useState} from "react";
+import clientNews from "../../lib/algoliaService";
+import {mapNewsItem} from "../../utils/utils";
+import {INewsData} from "../../types/types";
+
+
+export const FilterSearch = ({setFilteredNews, view}: {setFilteredNews: Dispatch<SetStateAction<INewsData[]>>; view: string}) => {
+    const [query, setQuery] = useState('');
+    const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setQuery(event.target.value)
+    }
+
+    const getFilteredNews = async () =>{
+        const { hits } = await clientNews.initIndex('news').search("", {
+            filters: `topics.title:${query}`,
+        })
+        const mappedHits = hits.map(mapNewsItem)
+        setFilteredNews(mappedHits)
+    }
+
+
+//styles to be adjusted
+    return (
+        <div className={`${styles.filter_container} ${styles[view]}`}>
+            {view === 'list' && <span>Filter & Refine</span>}
+            <TextField
+                label="search"
+                onInput={handleInput}
+                value={query}
+                InputProps={{
+                    endAdornment: (
+                        <InputAdornment position="start">
+                            <IconButton onClick={getFilteredNews} disabled={!query}>
+                                <SearchIcon />
+                            </IconButton>
+                        </InputAdornment>
+                    )
+                }}
+            />
+        </div>
+    )
+}
+
